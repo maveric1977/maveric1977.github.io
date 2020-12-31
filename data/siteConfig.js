b8c60de9c0d8569b86dadcc8d4baf579b869e4ba
Yashar Moradi <yashar.moradi@scoperty.de>
2020-12-31 01:30:27 +0100

    TM icon

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -120,7 +120,7 @@ module.exports = {
   fontColor: "#000000cc",
   enableDarkmode: true, // If true, enables dark mode switch
   display: 'minimal-ui',
-  icon: 'src/assets/gatsby-icon.png',
+  icon: 'src/assets/ym-icon.png',
   headerLinks: [
   ]
 }
